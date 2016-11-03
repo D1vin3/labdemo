d6169c1318f71cdfa92eca6a96f8258f74095752
d1vin3 <temirlan.nurmakhanov@is.sdu.edu.kz>
2016-11-03 23:08:10 +0600

    es

--- labdemo/management/commands/push_to_index.py
+++ labdemo/management/commands/push_to_index.py
@@ -1,10 +1,127 @@
+# -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
-
+from labdemo.models import kazpost
+from elasticsearch.client import IndicesClient
+from django.conf import settings
+# from labdemo.es_mappings import model_es_indices, es_mappings
+from elasticsearch.helpers import bulk
+from django.contrib.gis import geos
+import json
 
 class Command(BaseCommand):
+    es_index_name = 'labdemo'
+    es_ind_settings = {
+        "settings": {
+            "analysis": {
+                "analyzer": {
+                    "my_analyzer": {
+                        "type": "custom",
+                        "tokenizer": "standard",
+                        "filter": ["lowercase", "my_stopwords", "standard"]
+                    }
+                },
+                "filter": {
+                    "my_stopwords": {
+                        "type": "stop",
+                        "stopwords": "дом,строение,район,город,улица,область"
+                    }
+                }
+            }
+        }
+    }
 
 
-	def handle(self, *args, **options):
-		print "hello"
+	# def handle(self, *args, **options):
+	# 	# data = kazpost.objects.all()[:10];
+	# 	# print data
+	# 	for i in kazpost.objects.all()[:10]:
+	# 		print i
 
 
+
+    def handle(self, *args, **options):
+        self.recreate_index()
+        self.push_db_to_index()
+ 
+    def push_db_to_index(self):
+        data = [
+            self.convert_for_bulk(s, 'create') for s in kazpost.objects.all()[:1000]
+            ]
+        bulk(client=settings.ES_CLIENT, actions=data, stats_only=True)
+ 
+    def convert_for_bulk(self, django_object, action=None):
+        data = django_object.es_repr()
+        metadata = {
+            '_op_type': action,
+            "_index": model_es_indices[django_object.__class__.__name__]['index_name'],
+            "_type": model_es_indices[django_object.__class__.__name__]['type'],
+        }
+        data.update(**metadata)
+        return data
+ 
+    def recreate_index(self):
+        indices_client = IndicesClient(client=settings.ES_CLIENT)
+        index_name = self.es_index_name
+        if indices_client.exists(index_name):
+            indices_client.delete(index=index_name)
+        indices_client.create(index=index_name, body = self.es_ind_settings)
+ 
+        ## create mapping for one model only for now
+        model_name = 'kazpost'
+        indices_client.put_mapping(
+            doc_type=model_es_indices[model_name]['type'],
+            body=es_mappings[model_name],
+            index=index_name
+        )
+
+
+
+
+es_mappings = {
+    "kazpost": {
+        "properties": {
+            "region": {
+                "type": "string",
+                "analyzer": "my_analyzer"
+            },
+            'area': {
+                "type": "string",
+                "analyzer": "my_analyzer"
+            },
+            'locality': {
+                "type": "string",
+                "analyzer": "my_analyzer"
+            },
+            'district': {
+                "type": "string",
+                "analyzer": "my_analyzer"
+            },
+            'street': {
+                "type": "string",
+                "analyzer": "my_analyzer"
+            },
+            'house': {
+                "type": "string",
+                "analyzer": "my_analyzer"
+            },
+        }
+    }
+}
+
+model_es_indices = {
+    "kazpost": {
+        'index_name': "labdemo",
+        "type": "kazpost"
+    }
+}
+ 
+fields_weights = {
+    'locality': 5,
+    'street': 3,
+    'house': 2
+}
+fuzzy_fields_weights = {
+    'locality': 1,
+    'street': 1,
+    'house': 1
+}
--- labdemo/models.py
+++ labdemo/models.py
@@ -2,6 +2,38 @@ from django.contrib.gis import geos
 from django.contrib.gis.db import models
 
 
+es_mappings = {
+    "kazpost": {
+        "properties": {
+            "region": {
+                "type": "string",
+                "analyzer": "my_analyzer"
+            },
+            'area': {
+                "type": "string",
+                "analyzer": "my_analyzer"
+            },
+            'locality': {
+                "type": "string",
+                "analyzer": "my_analyzer"
+            },
+            'district': {
+                "type": "string",
+                "analyzer": "my_analyzer"
+            },
+            'street': {
+                "type": "string",
+                "analyzer": "my_analyzer"
+            },
+            'house': {
+                "type": "string",
+                "analyzer": "my_analyzer"
+            },
+        }
+    }
+}
+
+
 class kazpost(models.Model):
     post_index = models.CharField(max_length=10, blank=True, null=True)
     region = models.CharField(max_length=50, blank=True, null=True)
@@ -15,5 +47,44 @@ class kazpost(models.Model):
     lon = models.FloatField(blank=True, null=True)
 
     def __unicode__(self):
-        name = self.locality+' '+self.district+' '+self.street+' '+self.street+' '+self.house
+        name = ' '
+        if self.locality:
+            name += self.locality+' '
+        if self.district:
+            name += self.district
+        if self.street:
+            name += self.street
+        if self.house:
+            name += self.house
         return unicode(name)
+    
+    def es_repr(self):
+        data = {}
+        mapping = es_mappings[self.__class__.__name__]
+        data['_id'] = self.pk
+        for field_name in mapping['properties'].keys():
+            data[field_name] = self.field_es_repr(field_name)
+        return data
+ 
+    def field_es_repr(self, field_name):
+        mapping = es_mappings[self.__class__.__name__]
+        config = mapping['properties'][field_name]
+ 
+        field_es_value = getattr(self, field_name)
+ 
+        return field_es_value
+ 
+ 
+    @classmethod
+    def get_es_index(cls):
+        return model_es_indices[cls.__name__]['index_name']
+ 
+    @classmethod
+    def get_es_type(cls):
+        return model_es_indices[cls.__name__]['type']
+
+
+
+
+
+
